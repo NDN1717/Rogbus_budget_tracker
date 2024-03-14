d3d0805b90a67698c367eacef65c5bc5196b5db1
NDN1717 <oeto.neyra.up@phinmaed.com>
2024-03-14 12:35:37 +0800

    routes

--- routes/api.php
+++ routes/api.php
@@ -31,11 +31,11 @@ Route::post('/login-retrofit', [AuthManager::class, 'loginRetrofit']);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/get-expense', [ExpenseController::class, 'getExpense']);
     Route::post('/post-expense', [ExpenseController::class, 'postExpense']);
-    Route::put('/update-expense', [ExpenseController::class, 'updateExpense']);
-    Route::delete('/delete-expense', [ExpenseController::class, 'deleteExpense']);
+    Route::put('/update-expense/{expense}', [ExpenseController::class, 'updateExpense']);
+    Route::delete('/delete-expense/{expense}', [ExpenseController::class, 'deleteExpense']);
 
     Route::get('/get-saving', [SavingController::class, 'getSaving']);
     Route::post('/post-saving', [SavingController::class, 'postSaving']);
-    Route::put('/update-saving', [SavingController::class, 'updateSaving']);
-    Route::delete('/delete-saving', [SavingController::class, 'deleteSaving']);
+    Route::put('/update-saving/{saving}', [SavingController::class, 'updateSaving']);
+    Route::delete('/delete-saving/{saving}', [SavingController::class, 'deleteSaving']);
 });
